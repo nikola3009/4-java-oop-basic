1261c5fb30d22175fb33c8c5cc00912f69cc2abd
Nicola3009 <polozov97@mail.ru>
2018-10-16 09:40:13 +0500

    Полозов Николай. task02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -2,6 +2,12 @@ package com.example.task02;
 
 public class Task02Main {
     public static void main(String[] args) {
-
+        TimeSpan t1 = new TimeSpan(11, 34, 34);
+        TimeSpan t2 = new TimeSpan(2,45,45);
+        System.out.println(t1.toString());
+        t1.add(t2);
+        System.out.println(t1.toString());
+        t1.subtract(t2);
+        System.out.println(t1.toString());
     }
 }
---
+++ task02/src/com/example/task02/TimeSpan.java
@@ -0,0 +1,79 @@
+package com.example.task02;
+
+
+public class TimeSpan {
+    private int hour;
+    private int minute;
+    private int second;
+
+    public TimeSpan() {
+        hour = 0;
+        minute = 0;
+        second = 0;
+    }
+
+    public TimeSpan(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public void add(TimeSpan time) {
+        this.second += time.second;
+        if (this.second > 60){
+            this.minute += this.second /60;
+            this.second %= 60;
+        }
+        this.minute += time.minute;
+        if (this.minute > 60){
+            this.hour += this.minute /60;
+            this.minute %= 60;
+        }
+        this.hour += time.hour;
+    }
+
+    public void subtract(TimeSpan time) {
+        this.second -= time.second;
+        if (this.second < 0) {
+            this.second += 60;
+            this.minute--;
+        }
+        this.minute -= time.minute;
+        if (this.minute < 0) {
+            this.minute += 60;
+            this.hour--;
+        }
+        this.hour -= time.hour;
+        if (this.hour < 0){
+            System.out.println("Error");
+        }
+    }
+
+    public String toString(){
+        return String.format("%d:%d:%d", this.hour, this.minute, this.second);
+    }
+
+    public int getHours() {
+        return hour;
+    }
+
+    public int getMinutes() {
+        return minute;
+    }
+
+    public int getSeconds() {
+        return second;
+    }
+
+    public void setHours(int hours) {
+        this.hour = hours;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minute = minutes;
+    }
+
+    public void setSeconds(int seconds) {
+        this.second = seconds;
+    }
+}
